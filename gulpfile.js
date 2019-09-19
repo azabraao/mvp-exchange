366a6a52a598449967540ac05ca585914ab80f62
azabraao <az.abraao@gmail.com>
2019-09-19 13:13:33 -0300

    Change gulp sass task

--- gulpfile.js
+++ gulpfile.js
@@ -45,11 +45,22 @@ gulp.task('sprite', async () => {
 gulp.task('sass', async () => {
     gulp.src('./src/assets/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/assets/scss'));
+    .pipe(gulp.dest('./src/assets/scss/'))
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(concat('main.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./src/assets/css/'))    
+    
+    .pipe(uglifycss({
+        "uglyComments": true
+    }))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('./src/assets/css/'));    
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./src/assets/scss/main.css', gulp.series(['css']));
+    // gulp.watch('./src/assets/scss/main.css', gulp.series(['css']));
     gulp.watch('./src/assets/scss/**/*.scss', gulp.series(['sass']));
     gulp.watch('./src/assets/sprite/items/*.png', gulp.series(['sprite']));
     gulp.watch('./src/assets/img/fixture/*', gulp.series(['image']));
