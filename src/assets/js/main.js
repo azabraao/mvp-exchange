65c86707ecf560b0108021c9dcb9368281084df0
azabraao <az.abraao@gmail.com>
2019-09-23 14:31:14 -0300

    Add parceiros slide

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -20,7 +20,54 @@ const faq =  {
     }
 }
 
+const slide = {
+    init() {
+        // $('.jsParceirosSlide').slick({
+        //     slidesToShow: 2,
+        //     slidesToScroll: 2,
+        //     arrows: false,
+        //     fade: true,
+        // });
+        $('.jsParceirosSlide').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 6,
+            slidesToScroll: 4,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+    }
+}
+
 $(document).ready(function() {
     nav.init();
     faq.init();
+    slide.init();
 })
